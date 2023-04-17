fc34dbbc0eb3153043cac5199b6851eca320a904
David Shilman <shilmantech@gmail.com>
2023-04-16 20:12:02 -0400

    added logger

--- llm_api/api/views.py
+++ llm_api/api/views.py
@@ -1,4 +1,6 @@
 import os
+import logging
+import traceback
 
 from django.conf import settings
 from django.http import JsonResponse
@@ -9,6 +11,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
+logger = logging.getLogger("Views")
 class AskView(View):
 
     def get(self, request, *args, **kwargs):
@@ -21,6 +24,8 @@ class AskView(View):
         openai_api_key = os.environ.get('OPENAI_API_KEY')
         index_file_path = os.path.join(settings.BASE_DIR, 'indexed_articles', 'django_custom_user_model.json')
         index = GPTSimpleVectorIndex.load_from_disk(index_file_path)
+
+        logger.debug ("Index Created")
         QA_PROMPT_TMPL = (
             "Hello, I have some context information for you:\n"
             "---------------------\n"
@@ -28,8 +33,17 @@ class AskView(View):
             "\n---------------------\n"
             "Based on this context, could you please help me understand the answer to this question: {query_str}?\n"
         )
+        answer = None
         QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
-        answer = index.query(query_str, text_qa_template=QA_PROMPT)
+        try:
+            answer = index.query(query_str, text_qa_template=QA_PROMPT)
+            logger.debug(f"Answer {answer.response}")
+        except Exception as e:
+            logger.error (f"Error while calling chatGPT: {e}")
+            tb = traceback.format_exc()
+            logger.error (f"Error trace: {tb}")
+
+
         return JsonResponse({'answer': answer.response})
 
     def post(self, request, *args, **kwargs):
