13f43ca336ffd78a71785e3893c24d50c4113906
David Shilman <shimnatech@gmail.com>
2023-06-11 13:57:13 -0400

    working flask app

--- app.py
+++ app.py
@@ -1,57 +1,69 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import os
+from pathlib import Path
 import logging
-import traceback
-
 from dotenv import load_dotenv
-from llama_index import GPTSimpleVectorIndex, QuestionAnswerPrompt, download_loader
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
 
 
 app = Flask(__name__)
+
+
+# Load the index from disk
+load_dotenv()
+
+index = None
+
+# set up the index, either load it from disk to create it on the fly
+def initialise_index():
+    global index
+
+    index_file = os.getenv("INDEX_FILE", "./index_files/index.json")
+    index_folder = os.getenv("LOAD_DIR", "./index_files")
+
+    file = Path(index_file)
+    print (f"file {file.name} exists: {file.exists}")
+
+    folder = Path(index_folder)
+    print (f"folder {folder.name} exists: {(folder.exists)}")
+
+    # if index_file and os.path.exists(index_file) and os.path.isfile(index_file):
+    if file.exists:
+        index = GPTSimpleVectorIndex.load_from_disk(index_file)
+    elif folder.exists:
+        documents = SimpleDirectoryReader(index_folder).load_data()
+        index = GPTSimpleVectorIndex().build_index_from_documents(documents=documents)
+    else:
+        raise Exception ("Missing index loction")
+
+    logging.debug ("Index Created")
+
+initialise_index()
 
 @app.route('/')
 def home():
    return render_template('index.html')
 
 
-@app.route("/api/ask",  methods=['GET'])
-def ask_question():
+@app.route("/api/query")
+def query():
+    global index
+
     query_str = request.args.get('question', None)
+    print (f"question: {query_str}")
     if not query_str:
-        return {"error": "Please provide a question."}
+        return jsonify({"error": "Please provide a question."})
 
-    # Load the index from disk
-    load_dotenv()
-    openai_api_key = os.environ.get('OPENAI_API_KEY')
-    
-    logging.debug ("path " + os.getcwd())
-    index_file_path = os.path.join(os.getcwd(), 'indexed_files', 'data.json')
-    index = GPTSimpleVectorIndex.load_from_disk(index_file_path)
-
-    logging.debug ("Index Created")
-    QA_PROMPT_TMPL = (
-        "Hello, I have some context information for you:\n"
-        "---------------------\n"
-        "{context_str}"
-        "\n---------------------\n"
-        "Based on this context, could you please help me understand the answer to this question: {query_str}?\n"
-    )
     answer = None
-    QA_PROMPT = QuestionAnswerPrompt(QA_PROMPT_TMPL)
     try:
-        answer = index.query(query_str, text_qa_template=QA_PROMPT)
-        logging.debug(f"Answer {answer.response}")
-        return {'answer': answer.response}
-
+        answer = index.query(query_str)
     except Exception as e:
-        logging.error (f"Error while calling chatGPT: {e}")
-        tb = traceback.format_exc()
-        logging.error (f"Error trace: {tb}")
-        return {'answer': e}
+        print (f"Exception: {e}")
+        return jsonify({'answer': e})
+
+    return jsonify({'answer': answer.response})
 
 
-    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
 
-
-if __name__ == "__main__":  # pragma: no cover
-    app.run(debug=True)
