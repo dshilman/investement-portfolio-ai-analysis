ba241ecbe510eff396877a8dc34daa49a4222bce
David Shilman <shilmantech@gmail.com>
2023-06-15 19:31:39 -0400

    web index

--- create_index_from_web.py
+++ create_index_from_web.py
@@ -1,17 +1,55 @@
+import json
 import os
 
+import requests
 from dotenv import load_dotenv
-from llama_index import VectorStoreIndex, download_loader
-
+from llama_index import SimpleWebPageReader, VectorStoreIndex
 
 load_dotenv()
 
-urls = []
-data_source = None
 
-SimpleWebPageReader = download_loader("SimpleWebPageReader")
+def create_index():
 
-loader = SimpleWebPageReader()
-documents = loader.load_data(urls=urls)
-index = VectorStoreIndex(documents)
-index.save_to_disk(f'./index_files/index_{data_source}.json')
+    tickers = "AAPL, IBM, TSLA, AMZN"
+    article_links = get_stock_news_feed(tickers)
+    create_index_from_web(article_links=article_links)
+
+def create_index_from_web(article_links):
+
+    path = "indexed_files"
+    data_source = "web_index"
+
+    documents = SimpleWebPageReader().load_data(urls=article_links)
+    index = VectorStoreIndex(documents)
+    index.save_to_disk(f'./{path}/index_{data_source}.json')
+
+def get_stock_news_feed(tickers):
+
+    print('inside get_news_feed')
+
+    url = os.environ.get('NEWS_API_URL')
+
+    querystring = {"symbol": tickers}
+
+    headers = {
+        "X-RapidAPI-Key": os.environ.get('X-RapidAPI-Key'),
+        "X-RapidAPI-Host": os.environ.get('X-RapidAPI-Host')
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+
+    news_feed = response.json()['item']
+
+    print(f"Market News API response for {tickers}:")
+    print(json.dumps(news_feed, indent=2))
+
+    articles = []
+    for article in news_feed:
+        articles.append(article['link'])
+
+    print('exiting get_news_feed')
+
+    return articles
+
+
+if __name__ == '__main__':
+    create_index()
